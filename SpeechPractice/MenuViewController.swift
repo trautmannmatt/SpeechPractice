1b1b42ac2882dfc33f9fbf94f4bf05c536067cfc
ejgrimm <egrimm@rollins.edu>
2020-11-28 15:21:55 -0500

    First Commit, got some navigation working.

---
+++ SpeechPractice/MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MenuViewController.swift
+//  SpeechPractice
+//
+//  Created by Eric Grimm on 11/28/20.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Settings"
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ SpeechPractice/SettingsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsViewController.swift
+//  SpeechPractice
+//
+//  Created by Eric Grimm on 11/28/20.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Settings"
+        
+        print("Settings page has loaded!")
+    }
+
+
+}
