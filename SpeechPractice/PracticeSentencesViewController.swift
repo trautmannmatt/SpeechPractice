18ff95d3306371fa4f30f8637d1441f5a72b01d5
ejgrimm <egrimm@rollins.edu>
2020-12-05 13:13:59 -0500

    renamed view controllers

---
+++ SpeechPractice/PracticeSentencesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PracticeSentencesViewController.swift
+//  SpeechPractice
+//
+//  Created by Eric Grimm on 11/28/20.
+//
+
+import UIKit
+
+class PracticeSentencesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Practice Sentences"
+        
+        print("Practice Sentences Has Loaded!")
+    }
+
+
+}
---
+++ SpeechPractice/PracticeSingleWordsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PracticeSingleWordsViewController.swift
+//  SpeechPractice
+//
+//  Created by Eric Grimm on 11/28/20.
+//
+
+import UIKit
+
+class PracticeSingleWordsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Practice Single Words"
+        
+        print("Practice Single Words has loaded!")
+    }
+
+
+}
